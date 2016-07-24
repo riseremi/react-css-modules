3cc8b1ffdd1cc882344f6abd2c29ca70ee4bca9e
Remi <riseremi@icloud.com>
2016-07-24 15:20:23 +0300

    Fix hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
